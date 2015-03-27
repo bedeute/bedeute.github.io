6d8c27d0d66ec8576869a562adcfb6065891306e
bedeute <bedeute@gmail.com>
2015-03-28 01:52:24 +0700

    update

--- demo/growingfx/scripts/app.js
+++ demo/growingfx/scripts/app.js
@@ -8,11 +8,36 @@
   'use strict';
 
   $(function() {
-		$('#navbar-light').affix({
-		    offset: {
-		      top:450
-		    }
-		});
+
+  });
+
+  $('#navbar-light').affix({
+      offset: {
+        top:450
+      }
+  });
+
+  $('body').scrollspy({
+    target: '#nav',
+  });
+
+  $("#nav ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top-120
+     }, 300, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
 
   });
 
